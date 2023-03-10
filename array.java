0af6ca4a758ec95ab705bd18bef9aa103f262eff
Krishna Chandra Maurya <91846306+imkrishna-23@users.noreply.github.com>
2023-03-10 22:39:14 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,62 @@
+// import java.util.Scanner;
+
+// public class array {
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+
+//         // int[] marks = new int[5];
+//         // marks[0] = 1;
+//         // marks[1] = 1;
+//         // marks[2] = 1;
+//         // marks[3] = 1;
+//         // marks[4] = 1;
+//         int[] marks = { 1, 2, 3, 4, 5 };
+//         for (int i = 0; i < 5; i++) {
+//             System.out.println(marks[i]);
+//         }
+//     }
+// }
+
+// import java.util.Scanner;
+
+// public class array {
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter the size of array :");
+//         int size = sc.nextInt();
+
+//         int number[] = new int[size];
+//         // input
+//         for (int i = 0; i < size; i++) {
+//             number[i] = sc.nextInt();
+//         }
+//         // output
+//         for (int i = 0; i < size; i++) {
+//             System.out.println(number[i]);
+//         }
+//     }
+// }
+
+// import java.util.Scanner;
+
+// public class array {
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter the size of array :");
+//         int size = sc.nextInt();
+//         System.out.println("Enter your array");
+//         int number[] = new int[size];
+//         // input
+//         for (int i = 0; i < size; i++) {
+//             number[i] = sc.nextInt();
+//         }
+//         System.out.println("Enter the number you want to search :");
+//         int x = sc.nextInt();
+//         // output
+//         for (int i = 0; i < size; i++) {
+//             if (number[i] == x) {
+//                 System.out.println(x + " " + "found at index" + " " + i);
+//             }
+//         }
+//     }
+// }
